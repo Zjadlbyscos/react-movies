1a1d1a530d2bbee9885d4baf52ee211019fea7c9
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-12 17:26:31 +0100

    Navlink and Routes added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
+
+import Home from 'pages/home/home';
+import Movies from 'pages/movie/movies';
 
 export const App = () => {
   return (
@@ -7,6 +10,15 @@ export const App = () => {
         <NavLink to="/">HOME</NavLink>
         <NavLink to="/movies">MOVIES</NavLink>
       </nav>
+      <Routes>
+
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="" element={<Home />}></Route>
+        <Route path="" element={<Home />}></Route>
+        <Route path="" element={<Home />}></Route>
+
+      </Routes>
     </div>
   );
 };
