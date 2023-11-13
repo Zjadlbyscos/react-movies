5a46059ae9f39cba0709c6dd6df8d3e5ea7a2ede
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-13 10:53:53 +0100

    Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
 
-import Home from 'pages/home/home';
-import Movies from 'pages/movie/movies';
+import Home from 'pages/home/Home';
+import Movies from 'pages/movie/Movies';
 
 export const App = () => {
   return (
@@ -11,13 +11,11 @@ export const App = () => {
         <NavLink to="/movies">MOVIES</NavLink>
       </nav>
       <Routes>
-
         <Route path="/" element={<Home />}></Route>
         <Route path="/movies" element={<Movies />}></Route>
         <Route path="" element={<Home />}></Route>
         <Route path="" element={<Home />}></Route>
         <Route path="" element={<Home />}></Route>
-
       </Routes>
     </div>
   );
