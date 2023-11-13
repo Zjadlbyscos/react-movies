043f70d5576ff4124450125fc60497f642075227
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-13 09:38:29 +0100

    Create getPopularMovies.js

---
+++ src/api/getPopularMovies.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_KEY = '854b2461bbe5507a6040236243f8820c';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const POPULAR_ENDPOINT = '/trending/all/day';
+
+const getPopularMovies = async () => {
+  const searchParams = new URLSearchParams({
+    api_key: API_KEY,
+    language: 'en-US',
+  });
+
+  const response = await axios.get(
+    `${BASE_URL}${POPULAR_ENDPOINT}?${searchParams}`
+  );
+  const data = await response.data;
+  const results = await data.results;
+
+  return results;
+} 
+
+
+
+export default getPopularMovies;
