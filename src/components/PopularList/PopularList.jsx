20c7253223b0f72384a0db93773e44975c5f8b12
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-13 10:02:17 +0100

    Create PopularList.jsx
    Component to home.jsx

---
+++ src/components/PopularList/PopularList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PopularList({ movies }) {
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+          <p>{movie.title}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default PopularList;
