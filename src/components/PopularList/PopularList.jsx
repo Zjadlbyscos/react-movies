e122924a7c41f1f6743f0bd59be8c57691772a05
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-13 10:54:17 +0100

    Prettier

--- src/components/PopularList/PopularList.jsx
+++ src/components/PopularList/PopularList.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function PopularList({ movies }) {
   return (
     <ul>
-      {movies.map((movie) => (
+      {movies.map(movie => (
         <li key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
           <p>{movie.title}</p>
         </li>
       ))}
