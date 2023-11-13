8f4e43ced16c24d26a1f82b916c2bcd90ba6c5af
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-13 09:53:05 +0100

    fetchPopularMovies - Home Page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,38 @@
+import React from "react";
+import { useEffect,useState } from "react";
+
+import getPopularMovies from "api/getPopularMovies";
 
 function Home() {
-  return(
-<div>
-    MOVIES TRANDING TODAY
-</div>
-  )  
+
+const [movies, setMovies] = useState([]);
+
+useEffect(() => {
+    const fetchPopularMovies = async () => {
+      try {
+        const trendingMovies = await getPopularMovies();
+        setMovies(trendingMovies);
+      } catch (error) {
+        console.error("Error fetching trending movies:", error.message);
+      }
+    };
+
+    fetchPopularMovies();
+  }, []);
+
+  return (
+    <div>
+      <h2>Movies Trending Today</h2>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            <p>{movie.title}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default Home;
