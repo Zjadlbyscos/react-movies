0ac0e534ca6f51ceebded7bec9139a3583431f4a
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-13 14:16:20 +0100

    Add cast to routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { NavLink, Route, Routes } from 'react-router-dom';
 import Home from 'pages/home/Home';
 import Movies from 'pages/movie/Movies';
 import MovieDetails from './movieDetails/MovieDetails';
+import Cast from './cast/Cast';
 //cast Reviews
 
 export const App = () => {
@@ -16,7 +17,7 @@ export const App = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/movies" element={<Movies />}></Route>
         <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="" element={<Home />}></Route>
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
         <Route path="*" element={<Home />}></Route>
       </Routes>
     </div>
