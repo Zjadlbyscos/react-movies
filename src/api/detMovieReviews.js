35e353981f0869e1456ff60dc4c7f78cddeaaba2
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-14 18:36:53 +0100

    Create detMovieReviews.js

---
+++ src/api/detMovieReviews.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { API_KEY, BASE_URL } from './KEY';
+import { async } from 'q';
+const MOVIE_REVIEWS_ENDPOINT = '/movie';
+
+const getMovieReviews = async movieId => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}${MOVIE_REVIEWS_ENDPOINT}/${movieId}/reviews?api_key=${API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching movie reviews:', error.message);
+    return null;
+  }
+};
