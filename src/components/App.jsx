4016007ed13725a63a16e41138002dcd5623169d
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-15 14:20:33 +0100

    hide app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,26 @@
-import {  Route, Routes } from 'react-router-dom';
-import SharedLayout from './sharedLayout/SharedLayout';
+// import {  Route, Routes } from 'react-router-dom';
+// import SharedLayout from './sharedLayout/SharedLayout';
 
-import Home from '../pages/Home/Home'
-import Movies from '../pages/Movie/Movies';
-import MovieDetails from './movieDetails/MovieDetails';
-import Cast from './cast/Cast';
-import Reviews from './reviews/Reviews';
+// //import Home from '../pages/Home/Home'
+// //import Movies from '../pages/Movie/Movies';
+// import MovieDetails from './movieDetails/MovieDetails';
+// import Cast from './cast/Cast';
+// import Reviews from './reviews/Reviews';
 
-export const App = () => {
-  return (
-    <div>
-     <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>  </Route>
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+// export const App = () => {
+//   return (
+//     <div>
+//      <Routes>
+//         <Route path="/" element={<SharedLayout />}>
+//           <Route index element={<Home />} />
+//           <Route path="/movies" element={<Movies />} />
+//           <Route path="/movies/:movieId" element={<MovieDetails />}>  </Route>
+//             <Route path="/movies/:movieId/cast" element={<Cast />} />
+//             <Route path="/movies/:movieId/reviews" element={<Reviews />} />
         
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes> 
-    </div>
-  );
-};
+//           <Route path="*" element={<Home />} />
+//         </Route>
+//       </Routes> 
+//     </div>
+//   );
+// };
