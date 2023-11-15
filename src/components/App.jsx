8e3c346ace7fb304220755e677a804874481566f
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-15 15:47:52 +0100

    code-splitting lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,27 @@
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
 import SharedLayout from './sharedLayout/SharedLayout';
 
-import Home from '../pages/Home/Home'
-import Movies from '../pages/Movie/Movies';
-import MovieDetails from './movieDetails/MovieDetails';
-import Cast from './cast/Cast';
-import Reviews from './reviews/Reviews';
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movie/Movies'));
+const MovieDetails = lazy(() => import('./movieDetails/MovieDetails'));
+const Cast = lazy(() => import('./cast/Cast'));
+const Reviews = lazy(() => import('./reviews/Reviews'));
 
 export const App = () => {
   return (
     <div>
-     <Routes>
+      <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>  </Route>
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-        
+          <Route path="/movies/:movieId" element={<MovieDetails />}></Route>
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
           <Route path="*" element={<Home />} />
         </Route>
-      </Routes> 
+      </Routes>
     </div>
   );
 };
