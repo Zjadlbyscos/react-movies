2ecc1268230d56625aadf1c254c879c329e57e77
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-15 14:24:27 +0100

    Reinstantement App.js /Home/Movies  to fix  bug

---
+++ src/pages/Movie/Movies.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { getMoviebyQuery } from 'api/getMovieByQuery';
+import SearchedList from 'components/SearchedList/SearchedList';
+
+
+
+function Movies() {
+  const [movies, setMovies] = useState([]);
+  const [searchParam, setSearchParam] = useState('');
+
+  const handleSearch = async e => {
+    e.preventDefault();
+
+    try {
+      const foundMovies = await getMoviebyQuery(searchParam);
+      setMovies(foundMovies);
+    } catch (error) {
+      console.error('Error searching movies:', error.message);
+      setMovies([]);
+    }
+  };
+
+  return (
+    <div>
+      <form  onSubmit={handleSearch}>
+        <input
+          name="query"
+       
+          placeholder="Search Movie"
+          value={searchParam}
+          onChange={e => setSearchParam(e.target.value)}
+        />
+        <button type="submit" >
+          Search
+        </button>
+      </form>
+
+      <SearchedList movies={movies} />
+    </div>
+  );
+}
+
+export default Movies;
